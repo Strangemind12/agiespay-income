d35f6b2e9b2d4b0b171ca7429e100e2db1653a15
Strangemind12 <143359170+Strangemind12@users.noreply.github.com>
2025-07-08 22:04:55 +0100

    Create Index.jsx

---
+++ routes/Index.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+// ... import more
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* more routes here */}
+      </Routes>
+    </Router>
+  );
+}
