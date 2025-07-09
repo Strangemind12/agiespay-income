c000ad355ea8770e357ebff1e0adb9f470206046
Strangemind12 <143359170+Strangemind12@users.noreply.github.com>
2025-07-09 07:34:02 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-// agiespay-income/frontend/vite.config.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // optional, for cleaner imports
+    },
+  },
   build: {
     outDir: 'dist',
-  },
-  server: {
-    port: 3000,
   },
 });
