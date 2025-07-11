862bcbf5b91a2b90b45e62acd57e3bf2898a09af
Strangemind12 <143359170+Strangemind12@users.noreply.github.com>
2025-07-11 02:40:51 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
+// src/App.jsx
+
 import React from 'react';
-import { AuthProvider } from './context/AuthContext';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
