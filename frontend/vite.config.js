0b93be0fbcd5025f42a45dee760b3b4108d70b3f
Strangemind12 <143359170+Strangemind12@users.noreply.github.com>
2025-07-11 02:43:39 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-// frontend/vite.config.js
+// vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Optional but handy
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
